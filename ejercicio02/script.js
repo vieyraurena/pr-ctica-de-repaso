f948ed46fb7fa685e7b069d34f78c3cb267e68c0
Vieyra <vieyraurenabravo@gmail.com>
2020-07-15 16:18:33 -0600

    Agrego ejercicio 02 sobre averiguar la suma desde el 1 hasta el número que se digitó

---
+++ ejercicio02/script.js
@@ -0,0 +1,7 @@
+let num = parseFloat(prompt('Ingrese un número'));
+let resultado = 0;
+for (let i = 1; i <= num; i++){
+  resultado += i;    
+}
+console.log(`La suma total, desde 1 hasta el número que digitó es de: ${resultado}`);
+
